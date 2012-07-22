78a5657ca934e50212bc9cbbca85b017b48140a2
angelsl <hidingfromhidden@gmail.com>
2012-07-22 21:53:06 +0800

    reNX: Moved NullEnumerator to Assembine.
    Signed-off-by: angelsl <hidingfromhidden@gmail.com>

--- Util.cs
+++ Util.cs
@@ -37,60 +37,6 @@ using System.Runtime.InteropServices;
 
 namespace reNX
 {
-    internal class NullEnumerator<T> : IEnumerator<T>
-    {
-        /// <summary>
-        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-        /// </summary>
-        /// <filterpriority>2</filterpriority>
-        public void Dispose()
-        {
-        }
-
-        /// <summary>
-        /// Advances the enumerator to the next element of the collection.
-        /// </summary>
-        /// <returns>
-        /// true if the enumerator was successfully advanced to the next element; false if the enumerator has passed the end of the collection.
-        /// </returns>
-        /// <exception cref="T:System.InvalidOperationException">The collection was modified after the enumerator was created. </exception><filterpriority>2</filterpriority>
-        public bool MoveNext()
-        {
-            return false;
-        }
-
-        /// <summary>
-        /// Sets the enumerator to its initial position, which is before the first element in the collection.
-        /// </summary>
-        /// <exception cref="T:System.InvalidOperationException">The collection was modified after the enumerator was created. </exception><filterpriority>2</filterpriority>
-        public void Reset()
-        {
-        }
-
-        /// <summary>
-        /// Gets the element in the collection at the current position of the enumerator.
-        /// </summary>
-        /// <returns>
-        /// The element in the collection at the current position of the enumerator.
-        /// </returns>
-        public T Current
-        {
-            get { throw new InvalidOperationException("Enumeration has passed the end of the collection."); }
-        }
-
-        /// <summary>
-        /// Gets the current element in the collection.
-        /// </summary>
-        /// <returns>
-        /// The current element in the collection.
-        /// </returns>
-        /// <exception cref="T:System.InvalidOperationException">The enumerator is positioned before the first element of the collection or after the last element.</exception><filterpriority>2</filterpriority>
-        object IEnumerator.Current
-        {
-            get { return Current; }
-        }
-    }
-
     internal static class Util
     {
         internal static T Die<T>(string cause)
