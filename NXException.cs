9919da86248673e0f11adad5400f60d9e74b39f4
angelsl <hidingfromhidden@gmail.com>
2013-06-09 15:21:02 +0800

    reNX: License update.
    Signed-off-by: angelsl <hidingfromhidden@gmail.com>

--- NXException.cs
+++ NXException.cs
@@ -15,17 +15,17 @@
 // You should have received a copy of the GNU General Public License
 // along with reNX. If not, see <http://www.gnu.org/licenses/>.
 // 
-// Linking this library statically or dynamically with other modules
-// is making a combined work based on this library. Thus, the terms and
+// Linking reNX statically or dynamically with other modules
+// is making a combined work based on reNX. Thus, the terms and
 // conditions of the GNU General Public License cover the whole combination.
 // 
-// As a special exception, the copyright holders of this library give you
-// permission to link this library with independent modules to produce an
+// As a special exception, the copyright holders of reNX give you
+// permission to link reNX with independent modules to produce an
 // executable, regardless of the license terms of these independent modules,
 // and to copy and distribute the resulting executable under terms of your
 // choice, provided that you also meet, for each linked independent module,
 // the terms and conditions of the license of that module. An independent
-// module is a module which is not derived from or based on this library.
+// module is a module which is not derived from or based on reNX.
 
 using System;
 
